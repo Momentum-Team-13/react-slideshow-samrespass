ff921c706bd1fa2d6f76352a6bde6438bec25205
samrespass <samrespass@gmail.com>
2022-07-20 11:29:20 -0400

    perfect now

--- src/slideshow.js
+++ src/slideshow.js
@@ -4,44 +4,47 @@ import axios from "axios"
 export default function Slider (props) {
     let [slide, setSlide] = useState(0);
     const [movie, setMovie] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
     axios
     .get("./ghibli-data.json")
     .then((response) => setMovie(response.data))
+    .then(setLoading(false))
     .catch(err=>console.log(err))
     
     }, []);
     // UseEffect Bullshit
+    if (loading === true) return "Loading..."
+    if (movie.length > 0) {
+    return (        <div className="container"  >
+        
+    <div className="movieImage"><img src={movie[slide].image} alt={movie[slide].title}></img></div>
 
-return (
-        <div className="container"  >
-        
-            <div className="movieImage"><img src={movie[slide].image} alt={movie[slide].title}></img></div>
-        
-       
-        <div className="movieText">
-            <h1>{movie[slide].title}</h1>
-            <h2>{movie[slide].original_title}</h2>
-            <p className="movieDescription">{movie[slide].description}</p>
-            <p className="release"><b>Released:</b> {movie[slide].release_date}</p>
-            <p className="runtime"> <b>Runtime: </b>{movie[slide].running_time} minutes</p>
-            { slide > 0 &&
-            <button className="first" onClick={() => setSlide(slide = 0)}>First</button>
-            }
-            { slide > 0 &&
-            <button className="prev" onClick={() => setSlide(slide - 1)}>Previous</button>
-            }
-            { slide < 21 &&
-            <button className="next" onClick={() => setSlide(slide + 1)}>Next</button>
-            }
-            { slide < 21 &&
-            <button className="last" onClick={() => setSlide(slide = 21)}>Last</button>
-            }
-        </div>
-        
-       
-        </div>
 
-)
+<div className="movieText">
+    <h1>{movie[slide].title}</h1>
+    <h2>{movie[slide].original_title}</h2>
+    <p className="movieDescription">{movie[slide].description}</p>
+    <p className="release"><b>Released:</b> {movie[slide].release_date}</p>
+    <p className="runtime"> <b>Runtime: </b>{movie[slide].running_time} minutes</p>
+    { slide > 0 &&
+    <button className="first" onClick={() => setSlide(slide = 0)}>First</button>
+    }
+    { slide > 0 &&
+    <button className="prev" onClick={() => setSlide(slide - 1)}>Previous</button>
+    }
+    { slide < 21 &&
+    <button className="next" onClick={() => setSlide(slide + 1)}>Next</button>
+    }
+    { slide < 21 &&
+    <button className="last" onClick={() => setSlide(slide = 21)}>Last</button>
+    }
+</div>
+
+
+</div>)
+
+    }
+
 }
